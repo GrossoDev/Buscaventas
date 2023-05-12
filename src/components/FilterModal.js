65d71c34991b8a3361bfdc3d3865b3e8d7a5846d
Giuliano Rosso <rossogiuliano@gmail.com>
2023-05-12 16:47:13 -0300

    Fixed type

--- src/components/FilterModal.js
+++ src/components/FilterModal.js
@@ -70,7 +70,7 @@ function FilterModal({ filteringQuery, onApply }) {
         <div className="modal-content">
           <div className="modal-header">
             <h1 className="modal-title fs-5" id="exampleModalLabel">{query.title}</h1>
-            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Cerrar" />
           </div>
 
           <div className="modal-body">
