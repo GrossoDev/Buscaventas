0746c9ddea3b17aa09f7b70c5cdf5b2e47a16cae
Giuliano Rosso <rossogiuliano@gmail.com>
2023-05-15 23:06:12 -0300

    Stylized queries

--- src/components/Queries.js
+++ src/components/Queries.js
@@ -3,10 +3,20 @@ import Query from './Query';
 
 function Queries({ queries, onFilter, onRemove }) {
   return (
-    <div>
-      {queries?.map(
-        (query) => <Query key={query.id} query={query} onFilter={onFilter} onRemove={onRemove} />
-      )}
+    <div className="container mt-4">
+      <p className="display-6 mb-4">Búsquedas</p>
+      {
+        queries?.length
+          ? queries.map(
+            (query) => <Query key={query.id} {...{ query, onFilter, onRemove }} />
+          )
+          : (
+            <>
+              <p>Haz una búsqueda en el cuadro de arriba para comenzar.</p>
+              <p>Cuando hagas al menos dos búsquedas, te mostraremos los vendedores disponibles.</p>
+            </>
+          )
+      }
     </div>
   );
 }
--- src/components/Query.js
+++ src/components/Query.js
@@ -1,75 +1,110 @@
 import React from 'react';
 
 function Query({ query, onFilter, onRemove }) {
-  // TODO: temporary style, replace later
-  const style = {
-    display: 'flex',
-    alignItems: 'center'
-  };
-
-  // TODO: join all the possible renders
   if (query.isPlaceholder) {
     return (
-      <div style={style}>
-        <img src="" alt="" />
+      <div className="d-flex mb-2">
+        <div className="flex-shrink-0">
+          <div className="img-thumbnail rounded d-flex align-items-center justify-content-center" style={{ width: '96px', height: '96px' }}>
+            <div className="spinner-border" role="status" />
+          </div>
+        </div>
 
-        <div>
-          <p>
-            {query.title}
-          </p>
-          <p>
-            Cargando...
+        <div className="flex-grow-1 ms-3 d-flex flex-column justify-content-center">
+          <span className="fs-5">{query.title}</span>
+          <p className="text-muted d-block">
+            Buscando...
           </p>
         </div>
 
-        <button type="button" onClick={() => onRemove(query.id)}>Cancelar búsqueda</button>
+        <div className="d-flex align-items-center">
+          <button
+            className="btn btn-outline-danger m-1"
+            type="button"
+            onClick={() => onRemove(query.id)}
+            aria-label="Quitar búsqueda"
+          >
+            <i className="bi bi-trash-fill" />
+          </button>
+        </div>
       </div>
     );
   }
 
-  if (!query.filteredResults?.length) {
-    return (
-      <div style={style}>
-        <img src="" alt="" />
-
-        <div>
-          <p>
-            No hay resultados para
-            {' '}
-            {query.title}
-          </p>
-        </div>
-
-        <button type="button" data-bs-toggle="modal" data-bs-target="#filterModal" onClick={() => onFilter(query.id)}>Filtrar</button>
-        <button type="button" onClick={() => onRemove(query.id)}>Quitar búsqueda</button>
-      </div>
-    );
-  }
-
-  const { thumbnail, price: minimumPrice } = query.filteredResults[0];
+  const firstResult = query.filteredResults[0];
+  const thumbnail = firstResult?.thumbnail;
+  const minimumPrice = firstResult?.price;
 
   return (
-    <div style={style}>
-      <img src={thumbnail} alt="" />
+    <div className="d-flex mb-2">
+      {
+      query.filteredResults?.length // If there are results in the query
+        ? (
+          <>
+            <div className="flex-shrink-0">
+              <img className="img-thumbnail object-fit-contain rounded" src={thumbnail} alt="" style={{ width: '96px', height: '96px' }} />
+            </div>
 
-      <div>
-        <p>{query.title}</p>
-        <p>
-          {query.filteredResults.length}
-          {query.filteredResults.length === 1 ? ' resultado' : ' resultados'}
-        </p>
+            <div className="flex-grow-1 ms-3 d-flex flex-column justify-content-center">
+              <span className="fs-5">{query.title}</span>
+              <p className="text-muted d-none d-md-block">
+                {query.filteredResults.length}
+                {query.filteredResults.length === 1 ? ' resultado' : ' resultados'}
+              </p>
+              <p className="me-2 d-block d-md-none align-items-center">
+                Empieza por
+                {' '}
+                <b>{`$ ${minimumPrice.toLocaleString()}`}</b>
+              </p>
+            </div>
+
+            <div className="me-2 d-none d-md-flex align-items-center">
+              <p className="m-0">
+                Empieza por
+                {' '}
+                <b>{`$ ${minimumPrice.toLocaleString()}`}</b>
+              </p>
+            </div>
+          </>
+        )
+        : (
+          <>
+            <div className="flex-shrink-0">
+              <div className="img-thumbnail rounded d-flex align-items-center justify-content-center" style={{ width: '96px', height: '96px' }}>
+                <i className="display-5 bi bi-question" />
+              </div>
+            </div>
+
+            <div className="flex-grow-1 ms-3 d-flex flex-column justify-content-center">
+              <span className="fs-5">{query.title}</span>
+              <p className="text-muted d-block">
+                No hay resultados
+              </p>
+            </div>
+          </>
+        )
+      }
+
+      <div className="d-flex align-items-center">
+        <button
+          className="btn btn-outline-success m-1"
+          type="button"
+          data-bs-toggle="modal"
+          data-bs-target="#filterModal"
+          onClick={() => onFilter(query.id)}
+          aria-label="Filtrar"
+        >
+          <i className="bi bi-funnel-fill" />
+        </button>
+        <button
+          className="btn btn-outline-danger m-1"
+          type="button"
+          onClick={() => onRemove(query.id)}
+          aria-label="Quitar búsqueda"
+        >
+          <i className="bi bi-trash-fill" />
+        </button>
       </div>
-
-      <div>
-        <p>
-          Empieza por $
-          {' '}
-          {minimumPrice}
-        </p>
-      </div>
-
-      <button type="button" data-bs-toggle="modal" data-bs-target="#filterModal" onClick={() => onFilter(query.id)}>Filtrar</button>
-      <button type="button" onClick={() => onRemove(query.id)}>Quitar búsqueda</button>
     </div>
   );
 }
