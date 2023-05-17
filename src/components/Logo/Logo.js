fd1866f82cd20c3af4866d57147bee322ff2122e
Giuliano Rosso <rossogiuliano@gmail.com>
2023-05-17 02:56:32 -0300

    Moved Logo to its own component

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Logo.css';
+
+function Logo() {
+  return <div className="logo" />;
+}
+
+export default Logo;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
+import Logo from '../Logo';
 import './Navbar.css';
 
 function Navbar() {
@@ -7,7 +8,7 @@ function Navbar() {
     <nav className="navbar navbar-expand-lg border-bottom">
       <div className="container py-0 py-lg-3">
         <a className="navbar-brand" href="#">
-          <div className="logo" />
+          <Logo />
         </a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
