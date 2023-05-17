ba000a2c7fcc018e06343cb2abe7bdc0dfa22f7c
Giuliano Rosso <rossogiuliano@gmail.com>
2023-05-17 02:13:18 -0300

    Navbar created

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg border-bottom">
+      <div className="container py-0 py-lg-3">
+        <a className="navbar-brand" href="#">
+          <div className="logo" />
+        </a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-4">
+            <li className="nav-item">
+              <a className="nav-link" aria-current="page" href="#">Inicio</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Proyectos Frontend
+                <i className="bi bi-caret-down-fill ms-2" />
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Buscaventas</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import Navbar from './components/Navbar';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Navbar />
     <App />
   </React.StrictMode>
 );
