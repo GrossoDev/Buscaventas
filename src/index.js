e8a9eb32e9814010db9962e077c000b2e21f1afe
Giuliano Rosso <rossogiuliano@gmail.com>
2023-05-18 17:04:28 -0300

    Started organizing elements

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Navbar from './grosso-bootreact/components/Navbar';
+import Footer from './grosso-bootreact/components/Footer';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
