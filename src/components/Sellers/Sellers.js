79abffccf640804dce8d775e6bbd65a554ebb71e
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-07 18:58:31 -0300

    Fixed inconsistent use of destructuring & comments

--- src/components/Sellers/Sellers.js
+++ src/components/Sellers/Sellers.js
@@ -13,18 +13,21 @@ function Sellers({ queries, onSelect }) {
     .filter((seller, index, array) => array.findIndex((value) => value.id === seller.id) === index);
 
   // Create seller-results pairs
-  // Make sure all sellers contain results for all queries
+  // Remove result-seller pairs with no results
+  // Add extra info about each seller
+  // Sort them by total price
   const resultsBySeller = sellers
     .map((seller) => ({
       seller,
       results: queriesReady
         .map((query) => query.filteredResults.find((result) => result.seller.id === seller.id))
     }))
-    .filter(({ results }) => results.every((v) => v)) // Remove result-seller pairs with no results
-    .map((pair) => ({
-      ...pair,
-      totalPrice: pair.results.reduce((total, result) => total + result.price, 0),
-      freeShipping: pair.results.some((result) => result.freeShipping)
+    .filter(({ results }) => results.every((v) => v))
+    .map(({ seller, results }) => ({
+      seller,
+      results,
+      totalPrice: results.reduce((total, result) => total + result.price, 0),
+      freeShipping: results.some((result) => result.freeShipping)
     }))
     .sort((a, b) => a.totalPrice - b.totalPrice);
 
