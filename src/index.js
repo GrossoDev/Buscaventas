7ca76d40ca8a38fb515ffd35c50941caf0aec95a
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-10 19:00:33 -0300

    Added analytics

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Navbar from './grosso-bootreact/components/Navbar';
 import Footer from './grosso-bootreact/components/Footer';
+import Analytics from './grosso-bootreact/utils/analytics';
 import './index.css';
+
+Analytics.visited();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
