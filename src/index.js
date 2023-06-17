cc93b2a84e01211ae2a40a1c99b3d9cc919d5774
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-17 02:30:22 -0300

    Added bootstrap-icons

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
