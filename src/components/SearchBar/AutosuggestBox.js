86a9dde72d8754268cb44ef7dc8d2d046c5498d7
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-17 03:33:41 -0300

    Extracted Suggestion into its own component

--- src/components/SearchBar/AutosuggestBox.js
+++ src/components/SearchBar/AutosuggestBox.js
@@ -1,32 +1,19 @@
 import React from 'react';
+import Suggestion from './Suggestion';
 import '../../helpers/strings';
 
 function AutosuggestBox({ queryText, suggestions, suggestionClick, selectedIndex }) {
   return (
     <div className={`container dropdown-menu position-absolute start-0 end-0 ${suggestions.length !== 0 ? 'show' : ''}`}>
       {
-        !queryText.isEmptyOrWhitespace() && (
-          suggestions.map((suggestion, index) => {
-            const beginning = suggestion.q.substring(0, suggestion.match_start);
-            const match = suggestion.q.substring(suggestion.match_start, suggestion.match_end);
-            const ending = suggestion.q.substring(suggestion.match_end);
-
-            return (
-              <button
-                key={suggestion.q}
-                type="button"
-                className={`btn dropdown-item py-2 ${index === selectedIndex ? 'active' : ''}`}
-                onMouseDown={() => suggestionClick(suggestion.q)}
-              >
-                <i className="bi bi-search me-2" />
-
-                { beginning.length > 0 && <strong>{beginning}</strong> }
-                { match }
-                { ending.length > 0 && <strong>{ending}</strong> }
-              </button>
-            );
-          })
-        )
+        !queryText.isEmptyOrWhitespace()
+        && suggestions.map((suggestion, index) => (
+          <Suggestion
+            suggestion={suggestion}
+            isSelected={index === selectedIndex}
+            onClick={suggestionClick}
+          />
+        ))
       }
     </div>
   );
---
+++ src/components/SearchBar/Suggestion.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Suggestion({ suggestion, isSelected, onClick }) {
+  const beginning = suggestion.q.substring(0, suggestion.match_start);
+  const match = suggestion.q.substring(suggestion.match_start, suggestion.match_end);
+  const ending = suggestion.q.substring(suggestion.match_end);
+
+  return (
+    <button
+      key={suggestion.q}
+      type="button"
+      className={`btn dropdown-item py-2 ${isSelected ? 'active' : ''}`}
+      onMouseDown={() => onClick(suggestion.q)}
+    >
+      <i className="bi bi-search me-2" />
+
+      {beginning.length > 0 && <strong>{beginning}</strong>}
+      {match}
+      {ending.length > 0 && <strong>{ending}</strong>}
+    </button>
+  );
+}
+
+export default Suggestion;
