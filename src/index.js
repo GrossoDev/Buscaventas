a59c6a50c6ce8d91a58a27dab765ec21ed089aab
Giuliano Rosso <rossogiuliano@gmail.com>
2023-06-17 02:14:51 -0300

    Added bootstrap import and removed CDN

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import Navbar from './grosso-bootreact/components/Navbar';
 import Footer from './grosso-bootreact/components/Footer';
 import Analytics from './grosso-bootreact/utils/analytics';
 import './index.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 Analytics.visited();
 
